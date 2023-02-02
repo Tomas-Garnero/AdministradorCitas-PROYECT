03d2e68e3fa478ec3567b5759be9c3ab8649c65a
unknown <tomygarnero9429@gmail.com>
2023-02-02 17:04:29 -0300

    Some changes

--- js/classes/UI.js
+++ js/classes/UI.js
@@ -34,6 +34,8 @@ class UI {
 
         this.limpiarHTML();
 
+        this.textoHeading(citas);
+
         citas.forEach(cita => {
             const {mascota, propietario, telefono, fecha, hora, sintomas, id} = cita;
 
@@ -82,10 +84,9 @@ class UI {
             const btnEditar = document.createElement("button");
             btnEditar.classList.add("btn", "btn-info");
             btnEditar.innerHTML = `Editar <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
-          </svg>
-          `
-          btnEditar.onclick = () => cargarEdicion(cita);
+            <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" /></svg>`
+
+            btnEditar.onclick = () => cargarEdicion(cita);
 
             // Agregar los parrafos al div Cita
             divCita.appendChild(mascotaParrafo);
